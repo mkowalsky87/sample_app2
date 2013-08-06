edfdf9d1d27efe747cf2653d5be6921aeeaaf2c5
Mike Kowalsky <innoender@gmail.com>
2013-08-06 16:18:57 -0500

    Add Contact page

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -38,6 +38,18 @@ describe "Static pages" do
       expect(page).to have_title('Ruby on Rails Tutorial Sample App2 | About Us')
     end
   end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App2 | Contact')
+    end
+  end
 end
 RSpec.configure do |config|
 
