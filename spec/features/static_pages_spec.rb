eabf943e1f26c254a8322a60b2d3eeecdb7a9817
Mike Kowalsky <innoender@gmail.com>
2013-08-06 16:55:34 -0500

    Added Guard and Spork

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App2" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App2'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App2 | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -23,7 +25,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App2 | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -35,7 +37,7 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App2 | About Us')
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
@@ -47,7 +49,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App2 | Contact')
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
